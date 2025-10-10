2d0e7da28e7f391d2965b1ca10a69178f0baed76
Jatin Sharma <jatina9410@gmail.com>
2025-10-10 19:26:35 +0530

    ABCD pattern 2nd method

--- ABCD _Pattern.js
+++ ABCD _Pattern.js
@@ -2,6 +2,23 @@ let n = 5;
 
 
 // 1st method
-for (let i = 1; i <=n; i++) {
-     
+for (let i = 1; i <= n; i++) {
+  let ch = 65;
+  for (let j = 1; j <=i; j++) {
+    let ascii = String.fromCharCode(ch)
+    process.stdout.write(ascii.trim()+' ');
+    ch++;
+  }
+  console.log();
+
+}
+
+
+for (let i = 1; i <= n; i++) {
+    let row = ''
+    for (let j = 1; j <= i; j++) {
+        row += String.fromCharCode(64 + j)+ " ";
+    }
+    console.log(row.trim());
+
 }
---
+++ Questions.md
@@ -0,0 +1,4 @@
+print a star pattern
+print a number pattern 1 12 123 1234
+prunt alsphabetical character A AB ABC ABCD
+Inverted Right angle 
