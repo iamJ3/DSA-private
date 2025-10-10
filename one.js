caa606c1a7fe68239501fe27ab473e8a1c3cebb4
Jatin Sharma <jatina9410@gmail.com>
2025-10-10 18:56:27 +0530

    number pattern

--- one.js
+++ one.js
@@ -1,1 +1,11 @@
-console.log('hey');
+
+// number star
+let n = 5;
+for (let i = 1; i <=n; i++) {
+    let str = ""
+    for (let j = 1; j <=i; j++) {        
+        process.stdout.write(j+"");
+    }
+    console.log()
+    
+}
